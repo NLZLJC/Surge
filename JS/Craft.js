e29a9589ba535fc760ce71af4bff1ebce6caf71f
Meet05 <69444254+NLZLJC@users.noreply.github.com>
2022-11-08 15:54:06 +0800

    Update Craft.js

--- JS/Craft.js
+++ JS/Craft.js
@@ -1,6 +1,9 @@
 let obj = JSON.parse($response.body);
+/*
 obj["subscription"]={
     "tier": "Pro",
     "subscriptionActive": true
   },
+*/
+obj["subscription"]={"tier":"Free","subscriptionActive":false},
 $done({body: JSON.stringify(obj)})
